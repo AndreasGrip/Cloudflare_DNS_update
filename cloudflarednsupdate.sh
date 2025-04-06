b5847cb80825f7f8156e3baca1e5a2d4dcd26a8a
Andreas Grip <andreas.grip@gmail.com>
2025-04-06 21:00:14 +0200

    Bugfix if multiple domains owned. Debug arg added. Smarter check of prerequirements.

--- cloudflarednsupdate.sh
+++ cloudflarednsupdate.sh
@@ -4,49 +4,85 @@ CLOUDFLAREEMAIL="example@gmail.com"
 CLOUDFLAREGLOBALKEY=""
 DOMAINTOUPDATE="www.example.org"
 
-# Check for ping
-if ! which ping >/dev/null 2>&1; then
-    echo "ping is not installed"
-    exit 1
-fi
-# Check for jq
-if ! which curl >/dev/null 2>&1; then
-    echo "curl is not installed"
-    exit 1
-fi
-# Check for jq
-if ! which jq >/dev/null 2>&1; then
-    echo "jq is not installed"
-    exit 1
-fi
+# Check for required tools
+for tool in ping curl jq; do
+    if ! which "$tool" >/dev/null 2>&1; then
+        echo "$tool is not installed"
+        exit 1
+    fi
+done
 
 DOMAINNAME=$(echo $DOMAINTOUPDATE | sed s/\.[0-9a-zA-Z\-]*\.[a-zA-Z]*$//g)
 if [[ "$DOMAINNAME" == "" ]] ; then
         DOMAINNAME="$DOMAINTOUPDATE"
 fi
 
-# echo "DOMAINNAME: $DOMAINNAME"
+if [[ "$1" == "debug" ]] ; then 
+    echo "DOMAINNAME: $DOMAINNAME"
+fi
+
+DOMAIN=$(echo $DOMAINTOUPDATE | sed 's/^[^.]*\.//')
+
+if [[ "$1" == "debug" ]] ; then 
+    echo "DOMAIN: $DOMAIN"
+fi
+
 DOMAINIP=$(ping -c 1 "$DOMAINTOUPDATE" | grep -oP '(\d{1,3}\.){3}\d{1,3}' | head -n 1)
-#echo "DOMAINIP: $DOMAINIP"
+
+if [[ "$1" == "debug" ]] ; then 
+    echo "DOMAINIP: $DOMAINIP"
+fi
+
 MYIP=$(curl -Ss https://api.ipify.org)
-#echo "MYIP: $MYIP"
+
+if [[ "$1" == "debug" ]] ; then 
+    echo "MYIP: $MYIP"
+fi
 
 if [[ "$DOMAINIP" == "$MYIP" ]] ; then
 	echo "No update needed"
 	exit 0
 fi
 
-exit 0
+ZONE=$(curl -Ss --request GET \
+                    --url https://api.cloudflare.com/client/v4/zones/ \
+                    --header 'Content-Type: application/json' \
+                    --header "X-Auth-Email: $CLOUDFLAREEMAIL" \
+                    --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" \
+                    | jq -r ".result[] | select(.name == \"$DOMAIN\") | .id")
 
-ZONE=$(curl -Ss --request GET --url https://api.cloudflare.com/client/v4/zones/ --header 'Content-Type: application/json' --header "X-Auth-Email: $CLOUDFLAREEMAIL" --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" | jq -r '.result[0].id')
-# echo "Zone: $ZONE" 
-DNSRECORDID=$(curl -Ss "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/" --header 'Content-Type: application/json' --header "X-Auth-Email: $CLOUDFLAREEMAIL" --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" | jq -r --arg DOMAINTOUPDATE "$DOMAINTOUPDATE" '.result | map(select(.type == "A" and .name == $DOMAINTOUPDATE)) | .[].id')
-# echo "DNSRECORDID: $DNSRECORDID"
+if [[ "$1" == "debug" ]] ; then 
+    echo "Zone: $ZONE" 
+fi
+
+if [[ "$ZONE" == "" ]] ; then 
+    echo "Failed to get zone for $DOMAIN"
+    exit 1
+fi
+
+DNSRECORDID=$(curl -Ss "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/" \
+                    --header 'Content-Type: application/json' \
+                    --header "X-Auth-Email: $CLOUDFLAREEMAIL" \
+                    --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" \
+                    | jq -r --arg DOMAINTOUPDATE "$DOMAINTOUPDATE" \
+                    '.result | map(select(.type == "A" and .name == $DOMAINTOUPDATE)) | .[].id')
+
+if [[ "$1" == "debug" ]] ; then 
+    echo "DNSRECORDID: $DNSRECORDID"
+fi
 
 DATA="{\"content\": \"$MYIP\",\"name\": \"$DOMAINNAME\",\"proxied\": false,\"type\": \"A\",\"ttl\": 600,\"id\": \"$DNSRECORDID\"}"
-# echo "DATA $DATA"
 
-SUCCESS=$(curl -sS -X PUT  --url https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$DNSRECORDID --header 'Content-Type: application/json' --header "X-Auth-Email: $CLOUDFLAREEMAIL" --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" -d "$DATA" | jq .success)
+if [[ "$1" == "debug" ]] ; then 
+    echo "DATA $DATA"
+fi
+
+SUCCESS=$(curl -sS -X PUT  --url https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$DNSRECORDID \
+                    --header 'Content-Type: application/json' \
+                    --header "X-Auth-Email: $CLOUDFLAREEMAIL" \
+                    --header "X-Auth-Key: $CLOUDFLAREGLOBALKEY" \
+                    -d "$DATA" \
+                    | jq .success)
 if "$SUCCESS" == "true" ; then 
 	echo "Success"
 	exit 0
